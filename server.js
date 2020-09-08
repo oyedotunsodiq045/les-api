571468163ebb0cd0391b36cdccf2356936288a88
Sodiq Oyedotun <oyedotunsodiq045@gmail.com>
2020-09-08 15:10:51 +0100

    setting up to push to Heroku

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use('/api/v1/admin/users', users);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5004;
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(
   PORT,
